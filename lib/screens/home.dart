7eff74155e2fdce1dea827c1fe55d67f11acb8bf
jesus <jesuscovam@icloud.com>
2019-09-22 21:02:01 -0500

    second commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,279 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'dart:async';
+import '../services/auth.dart';
+import '../models/todo.dart';
+
+
+class Home extends StatefulWidget {
+  Home({Key key, this.auth, this.onSignedOut, this.userId}) : super(key: key);
+  final BaseAuth auth;
+  final VoidCallback onSignedOut;
+  final String userId;
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Todo> _todoList;
+
+  final FirebaseDatabase _database = FirebaseDatabase.instance;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  final _textEditingController = TextEditingController();
+  StreamSubscription<Event> _onTodoAddedSubscription;
+  StreamSubscription<Event> _onTodoChangedSubscription;
+  
+  Query _todoQuery;
+
+  bool _isEmailVerified = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _checkEmailVerification();
+
+    _todoList = List();
+    _todoQuery = _database
+      .reference()
+      .child("todo")
+      .orderByChild("userId")
+      .equalTo(widget.userId);
+
+    _onTodoAddedSubscription = _todoQuery.onChildAdded.listen(_onEntryAdded);
+    _onTodoChangedSubscription = _todoQuery.onChildChanged.listen(_onEntryChanged);
+  }
+
+  void _checkEmailVerification() async {
+    _isEmailVerified = await widget.auth.isEmailVerified();
+    if (!_isEmailVerified){
+      _showVerifyEmailDialog();
+    }
+  }
+
+  void _resentVerifyEmail(){
+    widget.auth.sendEmailVerification();
+    _showVerifyEmailSentDialog();
+  }
+
+  void _showVerifyEmailDialog(){
+    showDialog(
+      context: context,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text('Verificar su cuenta'),
+          content: Text('Por favor verificar su cuenta con el link enviado a su correo'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Reenviar link'),
+              onPressed: (){
+                Navigator.of(context).pop();
+                _resentVerifyEmail();
+              },
+            ),
+            FlatButton(
+              child: Text('Regresar'),
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      }
+    );
+  }
+
+  void _showVerifyEmailSentDialog(){
+    showDialog(
+      context: context,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text('Verificar cuenta'),
+          content: Text('Link para verificar cuenta enviado a tu correo'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Regresar'),
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      }
+    );
+  }
+
+  @override
+  void dispose() {
+    _onTodoAddedSubscription.cancel();
+    _onTodoChangedSubscription.cancel();
+    super.dispose();
+
+  }
+
+  _onEntryChanged(Event event){
+    var oldEntry = _todoList.singleWhere((entry){
+      return entry.key == event.snapshot.key;
+    });
+
+    setState(() {
+      _todoList[_todoList.indexOf(oldEntry)] = Todo.fromSnapChot(event.snapshot);
+    });
+  }
+
+  _onEntryAdded(Event event){
+    setState(() {
+      _todoList.add(Todo.fromSnapshot(event.snapshot));
+    });
+  }
+
+  _signOut() async{
+    try{
+      await widget.auth.signOut();
+      widget.onSignedOut();
+    } catch(e){
+      print(e);
+    }
+  }
+
+  _addNewTodo(String todoItem){
+    if (todoItem.length > 0){
+      Todo todo = Todo(todoItem.toString(), widget.userId,false);
+      _database.reference().child("todo").push().set(todo.toJson());
+    }
+  }
+
+  _updateTodo(Todo todo){
+    todo.completed = !todo.completed;
+    if (todo != null){
+      _database.reference().child("todo").child(todo.key).set(todo.toJson());
+    }
+  }
+
+  _deleteTodo(String todoId, int index){
+    _database.reference().child("todo").child(todoId).remove().then((_){
+      print("$todoId eliminado");
+      setState((){
+        _todoList.removeAt(index);
+      });
+    });
+  }
+
+  _showDialog(BuildContext context) async{
+    _textEditingController.clear();
+    await showDialog<String>(
+      context: context,
+      builder: (BuildContext context){
+        return AlertDialog(
+          content: Row(
+            children: <Widget>[
+              Expanded(
+                child: TextField(
+                  controller: _textEditingController,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    labelText: 'Agregar todo'
+                  ),
+                ),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text('Cancelar'),
+              onPressed: (){
+                Navigator.pop(context);
+              },
+            ),
+            FlatButton(
+              child: const Text('Guardar'),
+              onPressed: (){
+                _addNewTodo(_textEditingController.text.toString());
+                Navigator.pop(context);
+              },
+            )
+          ],
+        );
+      }
+    );
+  }
+
+
+  Widget _showTodoList(){
+    if(_todoList.isNotEmpty){
+      return ListView.builder(
+        shrinkWrap: true,
+        itemCount: _todoList.length,
+        itemBuilder: (BuildContext context, int index){
+          String todoId = _todoList[index].key;
+          String subject = _todoList[index].subject;
+          bool completed = _todoList[index].completed;
+          String userId = _todoList[index].userId;
+          return Dismissible(
+            key: Key(todoId),
+            background: Container(color: Colors.red,),
+            onDismissed: (direction) async{
+              _deleteTodo(todoId, index);
+            },
+            child: ListTile(
+              title: Text(
+                subject,
+                style: TextStyle(fontSize: 20.0),
+              ),
+              trailing: IconButton(
+                icon: (completed)
+                  ? Icon(
+                    Icons.done_outline,
+                    color: Colors.green,
+                    size: 20.0,
+                    )
+                  : Icon(
+                    Icons.done,
+                    color: Colors.grey,
+                    size: 20.0,
+                  ),
+                onPressed: (){
+                  _updateTodo(_todoList[index]);
+                }),
+            ),
+          );
+        });
+    } else {
+      return Center(
+        child: Text(
+          'Bienvenido, tu lista esta vacia',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 30.0),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              'Cerrar Sesión',
+              style: TextStyle(
+                fontSize: 17.0,
+                color: Colors.white
+              )),
+              onPressed: _signOut,
+          )
+        ],
+      ),
+      body: _showTodoList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          _showDialog(context);
+        },
+        tooltip: 'Incrementar',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/screens/login.dart
@@ -0,0 +1,316 @@
+import 'package:flutter/material.dart';
+import '../services/auth.dart';
+
+enum FormMode {LOGIN, SIGNUP}
+
+class Login extends StatefulWidget {
+  Login({Key key, this.auth, this.onSignedIn}) : super(key: key);
+  
+  final BaseAuth auth;
+  final VoidCallback onSignedIn;
+
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _formKey = new GlobalKey<FormState>();
+  String _email;
+  String _password;
+  String _errorMessage;
+  
+  FormMode _formMode =FormMode.LOGIN;
+  bool _isLoading = false;
+
+  bool _validateAndSave(){
+    final form = _formKey.currentState;
+    if(form.validate()){
+      form.save();
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void _validateAndSubmit() async {
+    setState(() {
+      _errorMessage = "";
+      _isLoading = true;
+    });
+    if (_validateAndSave()){
+      String userId = "";
+      try{
+        if (_formMode == FormMode.LOGIN){
+          userId = await widget.auth.signIn(_email, _password);
+          print('Iniciar: $userId');
+        } else {
+          userId = await widget.auth.signUp(_email, _password);
+          widget.auth.sendEmailVerification();
+          _showVerifyEmailSentDialog();
+          print('Inicio como $userId');
+        }
+        setState(() {
+          _isLoading = false;
+        });
+
+        if (userId.length > 0 && userId !=null && _formMode == FormMode.LOGIN){
+          widget.onSignedIn();
+        }
+      } catch(e){
+        print('Error $e');
+        setState(() {
+          _isLoading = false;
+          _errorMessage = e.message;
+        });
+      }
+    }
+  }
+
+  @override
+  void initState() { 
+    _errorMessage ="";
+    _isLoading = false;
+    super.initState();
+    
+  }
+  Widget _showCircularProgress(){
+    if(_isLoading){
+      return Center(child: CircularProgressIndicator());
+    } return Container(height: 0.0, width: 0.0,);
+  }
+
+  Widget _showVerifyEmailSentDialog(){
+    showDialog(
+      context: context,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text('Verificar cuenta'),
+          content: Text('Link para verificar cuenta enviado a su correo'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Regresar'),
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        )
+      }
+    )
+  }
+
+  Widget _showLogo(){
+    return Hero(
+      tag: 'hero',
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+        child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 48.0,
+          child: FlutterLogo(
+            size: 48,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _showEmailInput(){
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 60.0, 0.0, 0.0),
+      child: TextFormField(
+        maxLines: 1,
+        keyboardType: TextInputType.emailAddress,
+        autofocus: false,
+        decoration: InputDecoration(
+          hintText: 'Correo',
+          icon: Icon(
+            Icons.mail,
+            color: Colors.grey,
+          )
+        ),
+        validator: (value)=> value.isEmpty ? 'Es necesario un correo' : null,
+        onSaved: (value)=> _email = value.trim(),
+      ),
+    );
+  }
+
+  Widget _showPasswordInput(){
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+      child: TextFormField(
+        maxLines: 1,
+        obscureText: true,
+        autofocus: false,
+        decoration: InputDecoration(
+          hintText: 'Contraseña',
+          icon: Icon(
+            Icons.lock,
+            color: Colors.grey,
+          )
+        ),
+        validator: (value) => value.isEmpty ? "Es necesaria una contraseña" : null,
+        onSaved: (value) => _password = value.trim(),
+      ),
+    );
+  }
+  
+  Widget _showPrimaryButton(){
+    return RaisedButton(
+      textColor: Colors.white,
+      padding: EdgeInsets.all(0.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: <Color>[
+                  Color(0xFF0D47A1),
+                  Color(0xFF1976D2),
+                  Color(0xFF42A5F5),
+            ],
+          )
+        ),
+        padding: EdgeInsets.all(10.0),
+        child: _formMode == FormMode.LOGIN
+          ? Text(
+            'Iniciar',
+            style: TextStyle(
+              fontSize: 20
+            ))
+          : Text(
+            'Crear Cuenta',
+            style: TextStyle(
+              fontSize: 20
+            ),
+          )
+      ),
+      onPressed: _validateAndSubmit,
+    );
+
+    // return Padding(
+    //   padding: EdgeInsets.fromLTRB(0.0, 45.0, 0.0, 0.0),
+    //   child: MaterialButton(
+    //     elevation: 5.0,
+    //     minWidth: 200.0,
+    //     height: 42,
+    //     color: Colors.blue,
+    //     child: _formMode == FormMode.LOGIN
+    //       ? Text(
+    //         'Iniciar',
+    //         style: TextStyle(
+    //           fontSize: 20.0,
+    //           color: Colors.white
+    //         ))
+    //       : Text(
+    //         'Crear Cuenta',
+    //         style: TextStyle(
+    //           fontSize: 20.0,
+    //           color: Colors.white
+    //         ),
+    //       ),
+    //       onPressed: (){},
+    //   ),
+    // );
+  }
+
+  Widget _showSecondaryButton(){
+    return RaisedButton(
+      textColor: Colors.black54,
+      child:  Container(
+        width: MediaQuery.of(context).size.width,
+        alignment: Alignment.center,
+        child: _formMode == FormMode.LOGIN
+        ? Text(
+          'Crear una cuenta',
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ))
+        : Text(
+          'Tienes una cuenta? Iniciar',
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+    ),
+    onPressed: _formMode == FormMode.LOGIN
+          ? _changeFormToSignUp
+          : _changeFormToLogin
+
+    );
+  }
+ 
+  void _changeFormToSignUp(){
+    _formKey.currentState.reset();
+    _errorMessage = "";
+    setState(() {
+      _formMode = FormMode.SIGNUP;
+    });
+  }
+
+  void _changeFormToLogin(){
+    _formKey.currentState.reset();
+    _errorMessage = "";
+    setState(() {
+      _formMode = FormMode.LOGIN;
+    });
+  }
+
+  Widget _showErrorMessage(){
+    if (_errorMessage.length > 0 && _errorMessage != null){
+      return Text(
+        _errorMessage,
+        style: TextStyle(
+          fontSize: 13.0,
+          color: Colors.red,
+          height: 1.0,
+          fontWeight: FontWeight.w300
+        ),
+      );
+    } else {
+      return Container(
+        height: 0.0,
+      );
+    }
+  }
+
+ 
+  Widget _showBody(){
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            _showLogo(),
+            _showEmailInput(),
+            _showPasswordInput(),
+            SizedBox(height: 30,),
+            _showPrimaryButton(),
+            SizedBox(height: 8,),
+            _showSecondaryButton(),
+            _showErrorMessage()
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          _showBody(),
+          _showCircularProgress()
+        ],
+      ),
+    );
+  }
+}
+
